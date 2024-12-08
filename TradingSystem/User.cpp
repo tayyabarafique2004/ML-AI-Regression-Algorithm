803f400286835bc708abb7bd33f42c7999c7555b
Tayyaba Rafique Qureshi <tayyabarafique204@gmail.com>
2024-12-08 21:24:50 +0500

    Added functions for hashing password, generating userId and generating verification code.

--- TradingSystem/User.cpp
+++ TradingSystem/User.cpp
@@ -1,30 +1,82 @@
 #include "User.h"
-
+#include <sql.h>
+#include <sqlext.h>
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 // Constructor
-User::User() : id(0), age(0) {}
-//Another
-User::User(int id,string userId,string username,string HashedPassword,int age,string email,int WalletId)
+User::User()  //id(0), age(0) 
 {
-    this->id=id;
+    this->age =0;
+}
+//Another
+User::User(string userId,string username,string HashedPassword,int age,string email,int WalletId)
+{
+    //this->id=id; 
     this->age=age;
     this->email=email;
     this->username=username;
     this->userId=userId;
-    this->hashedPassword=HashedPassword;
+    this->password=HashedPassword;
     this->WalletId=WalletId;    
 
 }
 // Destructor
 User::~User() {}
 
-// Getter and Setter for id
-int User::getId() const {
-    return id;
+void User::insertUserToDb()
+{
+
 }
 
-void User::setId(int id) {
-    this->id = id;
+string User::hashPassword(const string & password)
+{
+    //hash function
+   hash<string>hashfunction;
+   size_t hashedPassword= hashfunction(password);
+   //converting to hexadecimal
+   ostringstream hexastring;
+   hexastring<<hex<<hashedPassword;
+   return hexastring.str();
 }
+
+string User:: generateUserId(const string& userName)
+{
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<int> dist(100, 999); // 3-digit random number
+    int randomDigits = dist(generator);
+
+    string userId = userName.substr(0, 3); // First 3 characters of username
+    userId += to_string(randomDigits);
+    return userId;
+}
+
+string User::generateVerificationCode(int length)
+{
+    const std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<> dist(0, chars.size() - 1);
+
+    std::string code;
+    for (int i = 0; i < length; ++i) {
+        code += chars[dist(generator)];
+    }
+    return code;
+}
+
+// Getter and Setter for id
+// int User::getId() const {
+//     return id;
+// }
+
+// void User::setId(int id) {
+//     this->id = id;
+// }
 
 // Getter and Setter for username
 std::string User::getUsername() const {
@@ -37,11 +89,11 @@ void User::setUsername(const std::string& username) {
 
 // Getter and Setter for hashedPassword
 std::string User::getHashedPassword() const {
-    return hashedPassword;
+    return password;
 }
 
 void User::setHashedPassword(const std::string& hashedPassword) {
-    this->hashedPassword = hashedPassword;
+    this->password = password;
 }
 
 // Getter and Setter for email
@@ -62,6 +114,7 @@ void User::setAge(int age) {
     this->age = age;
 }
 
+
 int User::getWalletId() const {
     return WalletId;
 }
@@ -69,3 +122,4 @@ int User::getWalletId() const {
 void User::setWalletId(int walletId) {
     this->WalletId = walletId;
 }
+
--- TradingSystem/User.h
+++ TradingSystem/User.h
@@ -14,10 +14,10 @@ public:
      User();
      User(string userId,string username,string HashedPassword,int age,string email,int WalletId);
     ~User();
-
+     void insertUserToDb();//fun to insert data of user to db
     // Getters and Setters
- //   int getId() const;
- //  void setId(int id);
+    //   int getId() const;
+   //  void setId(int id);
     
     string getUsername() const;  
     void setUsername(const string& username);
@@ -31,21 +31,24 @@ public:
     int getAge() const;
     void setAge(int age);
 
-    string getUserId()const;
-    void setUserId(string userId);
+    //string getUserId()const;
+    //void setUserId(string userId);
  
   int getWalletId() const;
     void setWalletId(int walletId);
+    string generateVerificationCode(int length = 6);
 private:
 // No need since its auto incremental We could have a search/fetch function from DB 
  //   int id;      //oki
     string userId;          
     string username;       
-    string hashedPassword; 
+    string password; 
     string email;          
     int age;    
     int WalletId;  
-
+    //helper funs
+   string generateUserId(const string& userName);
+    string hashPassword(const string& password);
 };
 
 #endif
